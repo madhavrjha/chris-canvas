c726c05f8bf22f4fd5ba2b3706ab7d12727cceee
Madhav Jha <madhav.jha@samco.in>
2024-05-27 19:47:42 +0530

    my art - random color and random size circle on all screens

--- canvas.js
+++ canvas.js
@@ -21,3 +21,18 @@ c.strokeStyle = 'red'
 c.fillStyle = 'blue'
 c.fill()
 c.stroke()
+
+let colors = ['red', 'green', 'blue', 'black', 'gray']
+
+for (let i = 0; i <= 100; i++) {
+	let x = Math.random() * window.innerWidth
+	let y = Math.random() * window.innerHeight
+	let r = Math.floor(5 + Math.random() * 30)
+
+	c.beginPath()
+	c.arc(x, y, r, 0, Math.PI * 2)
+	c.fillStyle = colors[Math.floor(Math.random() * 4)]
+	c.fill()
+	c.strokeStyle = 'blue'
+	c.stroke()
+}
