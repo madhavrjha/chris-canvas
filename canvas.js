19a41d7fcbe9ed4c08d733775565c2b9296ed6ea
Madhav Jha <madhav.jha@samco.in>
2024-05-27 19:32:49 +0530

    line, path and arc shape added

--- canvas.js
+++ canvas.js
@@ -5,8 +5,19 @@ canvas.height = window.innerHeight
 
 let c = canvas.getContext('2d')
 
-for (let y = 100; y <= 800; y += 100) {
-	for (let x = 100; x <= 800; x += 100) {
-		c.fillRect(x, y, 50, 50)
-	}
-}
+c.fillStyle = 'orange'
+c.fillRect(100, 100, 300, 300)
+
+c.beginPath()
+c.moveTo(500, 700)
+c.lineTo(100, 300)
+c.lineTo(500, 800)
+c.strokeStyle = 'red'
+c.stroke()
+
+c.beginPath()
+c.arc(350, 350, 50, 0, 2 * Math.PI, false)
+c.strokeStyle = 'red'
+c.fillStyle = 'blue'
+c.fill()
+c.stroke()
